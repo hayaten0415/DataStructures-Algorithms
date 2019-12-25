082745f172e29c46aa6bf47e2a7b094e75abb81b
Hayashi Hironori <hayaten415@gmail.com>
2019-12-25 23:49:58 +0900

    Add Comment NextPermutation

--- NextPermutation/NextPermutation.java
+++ NextPermutation/NextPermutation.java
@@ -1,16 +1,21 @@
 class NextPermutation {
   public void nextPermutation(int[] nums) {
     int i = nums.length - 2;
+    //後ろから見ていって昇順が終わるギリギリまで探す
     while (i >= 0 && nums[i] >= nums[i + 1]) {
       i--;
     }
+
+    //昇順が終わる部分の前のインデックスを探す
     if (i >= 0) {
       int l = nums.length - 1;
       while (l >= 0 && nums[l] <= nums[i]) {
         l--;
       }
+      //昇順が終わる部分の境目になっている部分をswap
       swap(nums, i, l);
     }
+
     reverse(nums, i + 1);
   }
 
