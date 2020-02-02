e9c76c776d45d46310f95fdd63532a828670051a
Hayashi Hironori <hayaten415@gmail.com>
2020-02-02 13:54:49 +0900

    PrimeNumber

---
+++ PrimeNumber/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static boolean isPrime(int num) {
+    if (num < 2) return false;
+    else if (num == 2) return true;
+    else if (num % 2 == 0) return false; // 偶数はあらかじめ除く
+    double sqrtNum = Math.sqrt(num);
+    for (int i = 3; i <= sqrtNum; i += 2) {
+      if (num % i == 0) {
+        // 素数ではない
+        return false;
+      }
+    }
+    // 素数である
+    return true;
+  }
+}
